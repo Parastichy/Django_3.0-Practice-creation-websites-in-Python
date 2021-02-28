abc7368056337904d9f5092e456d45bfd7b35d96
Parastichy <erpuzb@gmail.com>
2021-02-28 19:10:30 +0500

    Обновленны параметры полей и модели

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render
 
 from .models import Bb
 
+
 def index(request):
     bbs = Bb.objects.order_by('-published')
     return render(request, 'bboard/index.html', {'bbs': bbs})
-    # template = loader.get_template('bboard/index.html')
-    # bbs = Bb.objects.order_by('-published')
-    # context = {'bbs': bbs}
-    # return HttpResponse(template.render(context, request))
-    # s = 'Список объявлений\r\n\r\n\r\n'
-    # for bb in Bb.objects.order_by('-published'):
-    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    # return HttpResponse(s, content_type='text/plan; charset=utf-8')
