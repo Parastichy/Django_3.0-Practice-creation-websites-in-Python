a169775210ce916e53568a863c537474a780a70b
Parastichy <erpuzb@gmail.com>
2021-02-28 11:40:56 +0500

    Использование функций сокращения render

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Bb
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
     # s = 'Список объявлений\r\n\r\n\r\n'
     # for bb in Bb.objects.order_by('-published'):
     #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
