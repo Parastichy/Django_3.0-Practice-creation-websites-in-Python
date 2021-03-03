47bcf8190369029dc34d80eb33037864e072bdbd
Parastichy <erpuzb@gmail.com>
2021-03-03 16:32:08 +0500

    Тесты по связям (один с одном) и (многие со многими)

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -52,6 +52,15 @@ class Rubric(models.Model):
         verbose_name = 'Рубрика'
         ordering = ['name']
 
+
 class AdvUser(models.Model):
     is_activated = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Spare(models.Model):
+    name = models.CharField(max_length=30)
+
+class Machine(models.Model):
+    name = models.CharField(max_length=30)
+    spares = models.ManyToManyField(Spare)
