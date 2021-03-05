45750ac7da2a62019c296c60793e43c7615970bd
Parastichy <erpuzb@gmail.com>
2021-03-05 11:19:20 +0500

    Рефакторинг.

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from .views import index, by_rubric, BbCreateView
 
+# Пример передачи контроллеру функций значений mode:
+# vals = {'mode': 'index'}
 urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
+
+    # Именованный маршрут
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+
+    # Передача значений
+    # path('<int:rubric_id>/', by_rubric, vals),
     path('', index, name='index'),
 ]
