d18476154c934095c9f3046a4daebcb5a90bebe5
Parastichy <erpuzb@gmail.com>
2021-03-10 19:13:27 +0500

    Создание формы путём полного объявления. Сложный вариант который позволяет описать поля формы во всех деталях.

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,16 +1,18 @@
 from django.forms import ModelForm, DecimalField, modelform_factory
 from django.forms.widgets import Select
-from .models import Bb
+from django import forms
+from .models import Bb, Rubric
+
 
 # Использование быстрого объявления формы связанной с моделью
-class BbForm(ModelForm):
-    class Meta:
-        model = Bb
-        fields = ('title', 'content', 'price', 'kind', 'rubric')
-        labels = {'title': 'Название товара'}
-        help_texts = {'rubric': 'Не забудьте задать рубрику!'}
-        field_classes = {'pruce': DecimalField}
-        widgets = {'rubric': Select(attrs={'size': 8})}
+# class BbForm(ModelForm):
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'kind', 'rubric')
+#         labels = {'title': 'Название товара'}
+#         help_texts = {'rubric': 'Не забудьте задать рубрику!'}
+#         field_classes = {'pruce': DecimalField}
+#         widgets = {'rubric': Select(attrs={'size': 8})}
 
 
 # Использование фабрики класов для создания формы
@@ -24,3 +26,18 @@ class BbForm(ModelForm):
 #                            field_classes={'price': DecimalField},
 #                            widgets={'rubric': Select(attrs={'size': 8})}
 #                            )
+
+
+# Создание формы путём полного объявления
+# Сложный вариант который позволяет описать поля формы во всех деталях
+class BbForm(forms.ModelForm):
+    title = forms.CharField(label='Название товара')
+    content = forms.CharField(label='Описание', widget=forms.widgets.Textarea())
+    price = forms.DecimalField(label='Цена', decimal_places=2)
+    rubric = forms.ModelChoiceField(queryset=Rubric.objects.all(), label='Рубрика',
+                                    help_text='Не забудьте выбрать рубрику!',
+                                    widget=forms.widgets.Select(attrs={'size': 8}))
+
+    class Meta:
+        model = Bb
+        fields = ('title', 'content', 'price', 'kind', 'rubric')
