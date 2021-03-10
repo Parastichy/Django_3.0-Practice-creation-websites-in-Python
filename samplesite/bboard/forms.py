0b6fdc27bdbba1e7eb68d9630630104b191919a3
Parastichy <erpuzb@gmail.com>
2021-03-10 18:48:06 +0500

    Использование фабрики классов для формы добовления объявления.

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm
-
+from django.forms import ModelForm, DecimalField, modelform_factory
+from django.forms.widgets import Select
 from .models import Bb
 
+# class BbForm(ModelForm):
+#     class Meta:
+#         model = Bb
+#         fields = ('title', 'content', 'price', 'kind', 'rubric')
 
-class BbForm(ModelForm):
-    class Meta:
-        model = Bb
-        fields = ('title', 'content', 'price', 'kind', 'rubric')
+# Использование фабрики класов
+BbForm = modelform_factory(Bb,
+                           fields=('title', 'content', 'price', 'rubric'),
+                           labels={'title': 'Название товара'},
+                           help_texts={'rubric': 'Не забудьте выбрать рубрику!'},
+                           field_classes={'price': DecimalField},
+                           widgets={'rubric': Select(attrs={'size': 8})}
+                           )
