1e73e4e0c1251894dcf6a16b2ff0b152e4ca662f
Parastichy <erpuzb@gmail.com>
2021-03-17 19:19:47 +0500

    Использование полиморфной связи.

--- samplesite/testapp/admin.py
+++ samplesite/testapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import AdvUser, Machine, Spare, Kit
+from .models import AdvUser, Machine, Spare, Kit, Note
 
 admin.site.register(AdvUser)
 admin.site.register(Machine)
 admin.site.register(Spare)
 admin.site.register(Kit)
+admin.site.register(Note)
--- samplesite/testapp/models.py
+++ samplesite/testapp/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class AdvUser(models.Model):
@@ -20,3 +22,11 @@ class Kit(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     spare = models.ForeignKey(Spare, on_delete=models.CASCADE)
     count = models.IntegerField()
+
+
+class Note(models.Model):
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
